2b61841eaa1bd40e41f8e6d55dd0ba587d1d6aec
Unknown <jumcorredorro@unal.edu.co>
2018-09-15 11:13:08 -0500

    graphql settings and maps

--- front/src/components/Map/Map.js
+++ front/src/components/Map/Map.js
@@ -5,12 +5,14 @@ const AnyReactComponent = ({ text }) => <div>{ text }</div>;
 export default class Map extends Component {
     static defaultProps = {
         center: { lat: 40.7446790, lng: -73.9485420 },
-        zoom: 11
+        zoom: 11,
+        api_key:"AIzaSyAIqsQUEeBs9L22hcqScPj98MnVvWKiQEA"
     }
 render() {
     return (
         <div className='google-map map-map'>
             <GoogleMapReact
+                bootstrapURLKeys={{ key: ["AIzaSyAIqsQUEeBs9L22hcqScPj98MnVvWKiQEA"] }}
                 defaultCenter={ this.props.center }
                 defaultZoom={ this.props.zoom }>
                 <AnyReactComponent
